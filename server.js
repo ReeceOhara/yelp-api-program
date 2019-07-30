b5b5117f5d9bb2dea937a5828f9f187a41a31369
Reece <ralexo99@gmail.com>
2019-07-30 18:56:01 -0400

    Add the dotenv package

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const yelp = require('yelp-fusion');
 const bodyParser = require('body-parser');
 const express = require('express');
+const dotenv = require('dotenv');
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+dotenv.config();
 
 //Setup static files
 app.use(express.static((__dirname + "/public")));
